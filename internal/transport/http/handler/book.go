dac028ae32b7e47ad86ca3f65a6bf58cae63a689
ynuraddi <definston@gmail.com>
2023-04-10 05:59:24 +0600

    added handler book

--- internal/transport/http/handler/book.go
+++ internal/transport/http/handler/book.go
@@ -1,71 +1,79 @@
 package handler
 
-// func (h *Manager) CreateBook(c echo.Context) error {
-// 	input := struct {
-// 		Title     string `json:"title"     validate:"required,min=5"`
-// 		Login    string `json:"login"    validate:"required,min=5"`
-// 		Password string `json:"password" validate:"required,min=5"`
-// 	}{}
+import (
+	"context"
+	"errors"
+	"net/http"
+	"time"
 
-// 	if err := c.Bind(&input); err != nil {
-// 		return c.JSON(http.StatusBadRequest, envelope{"error": "handler(CreateUser): bad request"})
-// 	}
+	"app/internal/model"
 
-// 	if err := c.Validate(input); err != nil {
-// 		return c.JSON(http.StatusBadRequest, envelope{
-// 			"error":         "handler(CreateUser): validation failed",
-// 			"errorValidate": err.Error(),
-// 		})
-// 	}
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
 
-// 	user := model.User{
-// 		Name:     input.Name,
-// 		Login:    input.Login,
-// 		Password: input.Password,
-// 	}
+func (h *Manager) CreateBook(c echo.Context) error {
+	input := struct {
+		Title  string `json:"title"     validate:"required,min=5"`
+		Author string `json:"author"    validate:"required,min=5"`
+	}{}
 
-// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-// 	defer cancel()
+	if err := c.Bind(&input); err != nil {
+		return c.JSON(http.StatusBadRequest, envelope{"error": "handler(CreateUser): bad request"})
+	}
 
-// 	// Я вот не знаю где и как лучше обрабатывать ошибки и как их лучше стандартизировать, не довело видеть хороших примеров, у меня не правильно мне кажеться сделано, хотелось бы знать как лучше.
+	if err := c.Validate(input); err != nil {
+		return c.JSON(http.StatusBadRequest, envelope{
+			"error":         "handler(CreateBook): validation failed",
+			"errorValidate": err.Error(),
+		})
+	}
 
-// 	err := h.s.User.Create(ctx, user)
-// 	switch {
-// 	case errors.Is(err, gorm.ErrDuplicatedKey):
-// 		return c.JSON(http.StatusUnprocessableEntity, envelope{"error": "handler(CreateUser): user already exist || " + err.Error()})
-// 	case err != nil:
-// 		return c.JSON(http.StatusInternalServerError, envelope{"error": "handler(CreateUser): " + err.Error()})
-// 	default:
-// 		return c.JSON(http.StatusCreated, envelope{"info": "user created"})
-// 	}
-// }
+	book := model.Book{
+		Title:  input.Title,
+		Author: input.Author,
+	}
 
-// func (h *Manager) GetUser(c echo.Context) error {
-// 	input := struct {
-// 		ID int `param:"id" validate:"required,min=1"`
-// 	}{}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-// 	if err := c.Bind(&input); err != nil {
-// 		return c.JSON(http.StatusBadRequest, envelope{"error": "handler(GetUser): bad request"})
-// 	}
+	err := h.s.Book.Create(ctx, book)
+	switch {
+	case errors.Is(err, gorm.ErrDuplicatedKey):
+		return c.JSON(http.StatusUnprocessableEntity, envelope{"error": "handler(CreateBook): book already exist || " + err.Error()})
+	case err != nil:
+		return c.JSON(http.StatusInternalServerError, envelope{"error": "handler(CreateBook): " + err.Error()})
+	default:
+		return c.JSON(http.StatusCreated, envelope{"info": "user created"})
+	}
+}
 
-// 	if err := c.Validate(input); err != nil {
-// 		return c.JSON(http.StatusBadRequest, envelope{
-// 			"error":         "handler(GetUser): validation failed",
-// 			"errorValidate": err.Error(),
-// 		})
-// 	}
+func (h *Manager) GetBook(c echo.Context) error {
+	input := struct {
+		ID int `param:"id" validate:"required,min=1"`
+	}{}
 
-// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-// 	defer cancel()
+	if err := c.Bind(&input); err != nil {
+		return c.JSON(http.StatusBadRequest, envelope{"error": "handler(GetUser): bad request"})
+	}
 
-// 	user, err := h.s.User.Get(ctx, input.ID)
-// 	switch {
-// 	case errors.Is(err, gorm.ErrRecordNotFound):
-// 		return c.JSON(http.StatusNotFound, envelope{"info": "handler(GetUser): user is not exist || " + err.Error()})
-// 	case err != nil:
-// 		return c.JSON(http.StatusInternalServerError, envelope{"error": "handler(GetUser): " + err.Error()})
-// 	default:
-// 		return c.JSON(http.StatusFound, user)
-// 	}
-// }
+	if err := c.Validate(input); err != nil {
+		return c.JSON(http.StatusBadRequest, envelope{
+			"error":         "handler(GetBook): validation failed",
+			"errorValidate": err.Error(),
+		})
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	user, err := h.s.Book.Get(ctx, input.ID)
+	switch {
+	case errors.Is(err, gorm.ErrRecordNotFound):
+		return c.JSON(http.StatusNotFound, envelope{"info": "handler(GetBook): book is not exist || " + err.Error()})
+	case err != nil:
+		return c.JSON(http.StatusInternalServerError, envelope{"error": "handler(GetBook): " + err.Error()})
+	default:
+		return c.JSON(http.StatusFound, user)
+	}
+}
